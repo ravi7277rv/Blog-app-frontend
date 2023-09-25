20e15001a97a8480a7903d12179311d7312ed07f
Ravi Kumar <ravikumar7277rv@gmail.com>
2023-09-25 22:49:05 +0530

    baseURL changes

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -2,12 +2,8 @@ import axios from 'axios';
 
 // Create an Axios instance with a base URL
 const Axios = axios.create({
-  baseURL: 'http://127.0.0.1:4040/api/v1', // Replace with your API base URL
-  // timeout: 10000, // Optional: Set a timeout for requests (in milliseconds)
-  // headers: {
-  //   'Content-Type': 'application/json',
-    
-  // },
+ // baseURL: 'http://127.0.0.1:4040/api/v1', // Replace with your API base URL http://127.0.0.1:1234
+ baseURL: 'http://127.0.0.1:1234/api/v1',
 });
 
 export default Axios;
