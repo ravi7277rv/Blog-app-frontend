a315a290d72fc4a0ec8c09b30a839248b2a072aa
Ravi Kumar <ravikumar7277rv@gmail.com>
2023-09-25 23:06:53 +0530

    baseURL updated live

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an Axios instance with a base URL
 const Axios = axios.create({
  // baseURL: 'http://127.0.0.1:4040/api/v1', // Replace with your API base URL http://127.0.0.1:1234
- baseURL: 'http://127.0.0.1:1234/api/v1',
+ baseURL: 'https://blog-app-nhgz.onrender.com/api/v1',
 });
 
 export default Axios;
